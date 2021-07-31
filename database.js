b729e63d0b12ee9a6ad631c2e09ba5200376f747
ram77-droid <ramm00324@gmail.com>
2021-07-31 19:27:27 +0530

    change

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 var url= "mongodb://localhost:27017/cibo";
-var onlineurl = "mongodb+srv://raam:ramm1234@cluster0.gihj1.mongodb.net/cibo?retryWrites=true&w=majority";
+var ram = "mongodb+srv://raam:ramm1234@cluster0.gihj1.mongodb.net/cibo?retryWrites=true&w=majority";
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
-mongoose.connect(onlineurl, { useNewUrlParser: true, useUnifiedTopology: true },function(err, db) {
+mongoose.connect(ram, { useNewUrlParser: true, useUnifiedTopology: true },function(err, db) {
   if (err) throw err;
   console.log("Database created!");
 });
