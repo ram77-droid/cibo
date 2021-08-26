5e96eefab7385f3f292e82efcd491f5db4517257
ram77-droid <ramm00324@gmail.com>
2021-08-26 10:32:18 +0530

    change

--- controller.js
+++ controller.js
@@ -3857,7 +3857,7 @@
                         order_status:1,
                         user_id:1,
                         order_number:1,
-                        item1:1
+                        item2:"$item1.user_id"
                     }
                 }
             ],function(err,success){
